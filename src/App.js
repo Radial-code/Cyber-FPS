b32b47d9a8ab14a711b115f24d26dbc6e1d06159
neharadialcode <neharadialcode@gmail.com>
2022-04-04 16:43:16 +0530

    code modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "./assets/styles/style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
 import Home from "./component/Home";
 import Loader from "./common/Loader";
