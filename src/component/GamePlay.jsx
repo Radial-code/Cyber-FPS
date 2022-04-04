92f306343e935c03327814e686d54d47d2f54964
neharadialcode <neharadialcode@gmail.com>
2022-04-04 12:53:16 +0530

    video added

--- src/component/GamePlay.jsx
+++ src/component/GamePlay.jsx
@@ -7,19 +7,21 @@ const GamePlay = () => {
         <div class="container">
           <div class="row justify-content-center modified-container">
             <div class="col-11 text-center">
-              <h2 class="fs-xl ff-audiowide fw-400 my-xl-5 pt-5 pb-4 position-relative">
+              <h2 class="fs-xl ff-audiowide fw-400 my-lg-5 mt-3  pt-5 pb-4 position-relative">
                 CyberFPS <span class="skyblue position-relative">Gameplay</span>
               </h2>
 
-              <video
-                class="w-100 mt-md-4 mt-xl-0"
-                autoplay
-                loop
-                src="./assets/video/mov_bbb.mp4"
-                type="video/mp4"
-              >
-                Your browser does not support the video tag.
-              </video>
+              <div className="video-container pt-3 pt-sm-5">
+                <iframe
+                  className="clip-path border-0"
+                  width="90%"
+                  height="473px"
+                  src="https://www.youtube.com/embed/J42SZXS-_Qo"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              </div>
             </div>
           </div>
         </div>
