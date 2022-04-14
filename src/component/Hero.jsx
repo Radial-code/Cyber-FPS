8d1001905283b70f31e3d0adf68d93d7b3a430ea
neharadialcode <neharadialcode@gmail.com>
2022-04-14 11:46:37 +0530

    code modified

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -10,7 +10,7 @@ export const Hero = () => {
         <Header />
         <div className="container modified-container mb-md-1 min-height-100vh d-flex flex-column justify-content-lg-center">
           <div className="row text-center text-lg-start align-items-center justify-content-center justify-content-lg-between">
-            <div className="col-12 col-lg-7 order-2 order-lg-1   animate__animated animate__fadeInRight animation__duration-3s animate__delay-2s hero-gif">
+            <div className="col-12 col-lg-8 col-xl-7  order-2 order-lg-1   animate__animated animate__fadeInRight animation__duration-3s animate__delay-2s hero-gif">
               <img className="w-100" src={ezgif} alt="ezgif" />
             </div>
             <div className="col-12  col-md-10 order-1 order-lg-2 col-lg-12  d-flex flex-column justify-content-end  text-lg-end   animate__animated animate__fadeInLeft animation__duration-3s animate__delay-2s">
